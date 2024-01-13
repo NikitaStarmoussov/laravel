28be4d7ffff50b3d4f1d7f7c9df2ed367c02aa30
NikitaStarmoussov <nikta.starmousov@gmail.com>
2024-01-13 22:17:27 +0500

    feat: basic update migration schema. Add users, directories and sub cards

--- database/migrations/2024_01_13_154011_create_subscribers_table.php
+++ database/migrations/2024_01_13_154011_create_subscribers_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,8 +14,8 @@ class CreateSubscribersTable extends Migration
     {
         Schema::create('subscribers', function (Blueprint $table) {
             $table->id();
-            $table->string('phone_number', 20);
-            $table->enum('registration_type', ['individual', 'organization']);
+            $table->string('phone_number');
+            $table->string('registration_type');
             $table->string('last_name')->nullable();
             $table->string('first_name')->nullable();
             $table->string('middle_name')->nullable();
@@ -25,8 +24,29 @@ class CreateSubscribersTable extends Migration
             $table->string('country');
             $table->string('city');
             $table->string('street');
-            $table->string('house_number');
-            $table->string('apartment_number');
+            $table->string('house_number')->nullable();
+            $table->string('apartment_number')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->timestamps();
+        });
+
+        Schema::create('directories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::create('subscriber_cards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('subscriber_id');
+            $table->foreign('subscriber_id')->references('id')->on('subscribers');
             $table->timestamps();
         });
     }
@@ -38,6 +58,9 @@ class CreateSubscribersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('subscriber_cards');
+        Schema::dropIfExists('directories');
+        Schema::dropIfExists('users');
         Schema::dropIfExists('subscribers');
     }
 }
